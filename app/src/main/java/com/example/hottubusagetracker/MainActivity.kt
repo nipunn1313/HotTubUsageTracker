3ad83e59b9d8200c5a388dbca50201e293678347
Nipunn Koorapati <nipunn1313@gmail.com>
2024-08-16 16:30:04 -0700

    Get the hot tub checks back

--- app/src/main/java/com/example/hottubusagetracker/MainActivity.kt
+++ app/src/main/java/com/example/hottubusagetracker/MainActivity.kt
@@ -4,24 +4,41 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.hottubusagetracker.ui.theme.HotTubUsageTrackerTheme
+import dev.convex.android.ConvexClient
+import io.github.cdimascio.dotenv.dotenv
+import kotlinx.serialization.Serializable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val dotenv = dotenv {
+            directory = "/assets"
+            filename = "env"
+        }
+        val convex = ConvexClient(dotenv["CONVEX_URL"])
         setContent {
             HotTubUsageTrackerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    Tasks(
+                        client = convex,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -30,18 +47,27 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+@Serializable
+data class Task(val _id: String,val text: String, val isCompleted: Boolean)
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    HotTubUsageTrackerTheme {
-        Greeting("Android")
+fun Tasks(client: ConvexClient, modifier: Modifier = Modifier) {
+    var tasks: List<Task> by remember { mutableStateOf(listOf()) }
+    LaunchedEffect(key1 = "launch") {
+        client.subscribe<List<Task>>("tasks:get").collect { result ->
+            result.onSuccess { remoteTasks ->
+                tasks = remoteTasks
+            }
+        }
+    }
+    LazyColumn(
+        modifier = modifier
+    ) {
+        items(tasks) { task ->
+            Row {
+                Checkbox(checked = task.isCompleted, onCheckedChange = {})
+                Text(text = task.text)
+            }
+        }
     }
 }
