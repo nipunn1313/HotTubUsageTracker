9762df6361b3ee6135e4b65e4f97078c37684221
Nipunn Koorapati <nipunn1313@gmail.com>
2024-08-16 16:33:16 -0700

    Add mutation

--- app/src/main/java/com/example/hottubusagetracker/MainActivity.kt
+++ app/src/main/java/com/example/hottubusagetracker/MainActivity.kt
@@ -17,12 +17,14 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.hottubusagetracker.ui.theme.HotTubUsageTrackerTheme
 import dev.convex.android.ConvexClient
 import io.github.cdimascio.dotenv.dotenv
+import kotlinx.coroutines.launch
 import kotlinx.serialization.Serializable
 
 class MainActivity : ComponentActivity() {
@@ -53,6 +55,8 @@ data class Task(val _id: String,val text: String, val isCompleted: Boolean)
 @Composable
 fun Tasks(client: ConvexClient, modifier: Modifier = Modifier) {
     var tasks: List<Task> by remember { mutableStateOf(listOf()) }
+    val coroutineScope = rememberCoroutineScope() // Create a coroutine scope tied to the composition
+
     LaunchedEffect(key1 = "launch") {
         client.subscribe<List<Task>>("tasks:get").collect { result ->
             result.onSuccess { remoteTasks ->
@@ -65,7 +69,11 @@ fun Tasks(client: ConvexClient, modifier: Modifier = Modifier) {
     ) {
         items(tasks) { task ->
             Row {
-                Checkbox(checked = task.isCompleted, onCheckedChange = {})
+                Checkbox(checked = task.isCompleted, onCheckedChange = { checked ->
+                    coroutineScope.launch {
+                        client.mutation("tasks:setCompleted", mapOf("taskId" to task._id, "completed" to checked))
+                    }
+                })
                 Text(text = task.text)
             }
         }
